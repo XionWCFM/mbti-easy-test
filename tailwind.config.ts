995157a91c248aff75d1e6d4f9737b7b928cfb07
devgiljong <calf-love@naver.com>
2023-10-07 19:26:41 +0900

    :sparkles: Tailwind config js 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,19 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
   ],
-  theme: {},
+  theme: {
+    borderRadius: {
+      sm: '10px',
+      md: '20px',
+      full: '999px',
+    },
+    boxShadow: {
+      default: `1px 1px 10px 1px hsla(264, 95%, 66%, 0.15)`,
+      light: `0px 1px 8px 0px hsla(264, 95%, 66%, 0.25)`,
+      dialog: `0px -8px 16px 0px hsla(0, 0%, 0%, 0.1)`,
+      base: `0px 1px 2px 0px hsla(0, 0%, 0%, 0.06) , 0px 1px 3px 0px hsla(0, 0%, 0%, 0.1)`,
+      card: `0px 4px 6px -2px hsla(0, 0%, 0%, 0.05) , 0px 10px 15px -3px hsla(0, 0%, 0%, 0.1)`,
+    },
+  },
   plugins: [],
 };
