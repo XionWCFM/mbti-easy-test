bf5751cc7fa72e467cf2bf136be7da3fdd371da2
devgiljong <calf-love@naver.com>
2023-10-08 10:26:21 +0900

    :sparkles: 아코디언

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import MainPage from '@/components/pages/main-page';
 import Numberpage from '@/components/pages/number-page';
+import AccoridionContainer from '@/components/provider/AccoridionContainer';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className=" ">
       <Link href={'/1'}>검사하기</Link>
+      <AccoridionContainer />
       <MainPage />
       <Numberpage />
     </main>
---
+++ components/provider/AccoridionContainer.tsx
@@ -0,0 +1,12 @@
+'use client';
+import * as AccordionPrimitive from '@radix-ui/react-accordion';
+
+import React from 'react';
+
+interface AccoridionContainerProps {}
+
+const AccoridionContainer = ({}: AccoridionContainerProps) => {
+  return <div>AccoridionContainer</div>;
+};
+
+export default AccoridionContainer;
