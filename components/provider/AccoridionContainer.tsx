a233d895222b398c50abc0204966839650fbe848
devgiljong <calf-love@naver.com>
2023-10-10 15:09:00 +0900

    :sparkles: 아코디언 해보기

--- components/provider/AccoridionContainer.tsx
+++ components/provider/AccoridionContainer.tsx
@@ -6,7 +6,18 @@ import React from 'react';
 interface AccoridionContainerProps {}
 
 const AccoridionContainer = ({}: AccoridionContainerProps) => {
-  return <div>AccoridionContainer</div>;
+  return (
+    <div>
+      <AccordionPrimitive.Root type="single" collapsible>
+        <AccordionPrimitive.Item value="item-1">
+          <AccordionPrimitive.Trigger>누르면 열려요</AccordionPrimitive.Trigger>
+          <AccordionPrimitive.Content>
+            여기 안에 콘텐츠가 들어가요
+          </AccordionPrimitive.Content>
+        </AccordionPrimitive.Item>
+      </AccordionPrimitive.Root>
+    </div>
+  );
 };
 
 export default AccoridionContainer;
