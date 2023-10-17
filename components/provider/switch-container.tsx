52dfe802e7f064c3651c40c4f8b868341976b97c
devgiljong <calf-love@naver.com>
2023-10-18 01:51:48 +0900

    :sparkles: Switch

--- components/provider/switch-container.tsx
+++ components/provider/switch-container.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import * as SwitchPrimitive from '@radix-ui/react-switch';
 
-interface SwitchContainerProps {}
+interface SwitchProps extends React.ComponentPropsWithRef<'div'> {
+  isCheck: boolean;
+}
 
-const SwitchContainer = ({}: SwitchContainerProps) => {
+const Switcher = (
+  { isCheck, ...props }: SwitchProps,
+  ref: React.Ref<HTMLDivElement>,
+) => {
   return (
-    <form action="">
-      <div className=" flex items-center">
-        <SwitchPrimitive.Root
-          className="w-[42px] h-[25px] rounded-full relative bg-purple-700  data-[state=checked]:bg-black outline-none cursor-default"
-          id="airplane-mode"
-        >
-          <SwitchPrimitive.Thumb
-            className={`
-          block w-[21px] h-[21px] bg-white rounded-full 
-          transition-transform duration-100 
-          translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]
-          `}
-          />
-        </SwitchPrimitive.Root>
-      </div>
-    </form>
+    <div
+      data-state={isCheck}
+      className=" w-[20px] h-[12px] rounded-full flex items-center bg-gray-30 data-[state=true]:bg-primary-100 transition-all duration-300 group"
+      {...props}
+    >
+      <button className=" w-[50%] h-[10px] bg-white rounded-full transition-all duration-300 translate-x-0.5 will-change-transform group-data-[state=true]:translate-x-[calc(100%-0.125rem)]"></button>
+    </div>
   );
 };
 
-export default SwitchContainer;
+export default Switcher;
